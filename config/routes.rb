5ef18a75f86681f2e2878c09d288cb52e6195af6
Diffat Elicka <903869629@qq.com>
2018-05-07 15:04:53 +0800

    stupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/ask', to: 'pages#ask'
+
+  get '/answer', to: 'pages#answer'
+
+  post '/ask/:question', to: 'pagescontroler#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
